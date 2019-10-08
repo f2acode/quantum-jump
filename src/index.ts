9a4e355a5c0eaf586b739b7f12b6c494cd3d21ba
Felipe Augusto <augustto.felipe@hotmail.com>
2019-10-07 21:32:48 -0300

    feat remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-//import { openBrowser } from "./helpers/browser";
 import { readConfigFile } from "./helpers/file";
 
 const config = readConfigFile()
 console.log(config)
-
-//openBrowser(['gitlab.com', 'reformma.com.br'])
 
 
